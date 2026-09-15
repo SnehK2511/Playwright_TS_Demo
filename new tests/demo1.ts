2d59190657be63fc0dcd63df34548477df5d6f8b
Sneh <rsneh00@gmail.com>
2026-09-15 21:13:40 +0530

    Updated TypeScript and Playwright framework notes

---
+++ new tests/demo1.ts
@@ -0,0 +1,19 @@
+let message1: string = "Hello, World!";
+message1 = "Hello, My nsame is TS";
+console.log(message1);
+
+
+let age: number =21;
+let Button: boolean = true;
+let numArry : number[] = [1,2,3,4,5];
+let StringArray: string[] = ["Hello", "World", "TypeScript"];
+let RandomArray: any[] = [1, "Hello", true, { name: "TypeScript" }];
+console.log(age);
+console.log(Button);
+console.log(numArry);
+console.log(StringArray);
+console.log(RandomArray);
+
+RandomArray.push("New Element");
+console.log(RandomArray);
+
